fba27ff4c96c75aa3c3ce924bf3e1e606e8a9d9a
Supermyke <supermyke97@gmail.com>
2025-08-03 23:29:36 +0100

    I change my dependency manager to uv

--- src/backend/app/models/database.py
+++ src/backend/app/models/database.py
@@ -13,7 +13,7 @@ load_dotenv(dotenv_path=path)
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME")
 
 DB_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
